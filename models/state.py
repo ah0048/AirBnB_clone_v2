7989c28d31125f9e166d11e4e853cc95ed192921
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-26 21:05:17 +0300

    Review class

--- models/state.py
+++ models/state.py
@@ -11,6 +11,7 @@ class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
         cities = relationship('City', backref='states', cascade="all, delete")
