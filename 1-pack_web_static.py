3cb1f964069091cc0c71c35c62ed0ed8d70fc6d6
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-12 20:03:16 +0300

    edit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz archive from the contents of the web_static'''
+from fabric import tasks
+from fabric.api import *
+from datetime import datetime
+
+
+@task
+def do_pack():
+    '''function to archive'''
+    directory = 'versions'
+    dir_to_archive = 'web_static'
+    time = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = f'{dir_to_archive}_{time}.tgz'
+    try:
+        local(f'mkdir -p {directory}')
+        local(f'tar czf {directory}/{archive_name} {dir_to_archive}')
+        path = f'{directory}/{archive_name}'
+        return path
+    except Exception:
+        return None
