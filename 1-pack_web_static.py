d1b14437dbd199db1c9e10b86f3826b4d83269ec
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-12 20:11:16 +0300

    edit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Fabric script that generates a .tgz archive from the contents of the web_static'''
+'''Fabric script that generates a .tgz archive of the web_static'''
 from fabric import tasks
 from fabric.api import *
 from datetime import datetime
