37f0355ecc3bce5d422b39e360279815e3c46933
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-13 05:33:50 +0300

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,12 +2,10 @@
 '''Fabric script that distributes an archive to your web servers'''
 from fabric.api import *
 import os
-from fabric import Connection
 
 
 env.hosts = ['100.26.53.100', '34.232.72.27']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+
 
 
 def do_deploy(archive_path):
@@ -16,17 +14,15 @@ def do_deploy(archive_path):
         return False
     try:
         for host in env.hosts:
-            conn = Connection(host=host, user=env.user, connect_kwargs={'key_filename': env.key_filename})
-            conn.put(archive_path, '/tmp/')
+            put(archive_path, '/tmp/')
             archive_name = os.path.basename(archive_path)
             exctraction_name = archive_name.split('.')[0]
-            conn.run(f'cd /tmp/{archive_name}')
-            conn.run(f'sudo tar -xzf {archive_name} -C /data/web_static/releases')
-            conn.run('cd /data/web_static/releases')
-            conn.run(f'mv web_static {exctraction_name}')
-            conn.run(f'sudo rm -r /tmp/{archive_name}')
-            conn.run('sudo rm /data/web_static/current')
-            conn.run(f'sudo ln -s /data/web_static/releases/{exctraction_name} /data/web_static/current')
+            run(f'sudo tar -xzf /tmp/{archive_name} -C /data/web_static/releases')
+            run('cd /data/web_static/releases')
+            run(f'mv /data/web_static/releases/web_static /data/web_static/releases/{exctraction_name}')
+            run(f'sudo rm -r /tmp/{archive_name}')
+            run('sudo rm /data/web_static/current')
+            run(f'sudo ln -s /data/web_static/releases/{exctraction_name} /data/web_static/current')
             return True
 
     except Exception:
