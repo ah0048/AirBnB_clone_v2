630c267ccd5976451b27c06119e9f28b7f16f810
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-13 06:04:02 +0300

    edit deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,23 +25,25 @@ def do_pack():
     except Exception:
         return None
 
+
 @task
 def do_deploy(archive_path):
     '''function to deploy'''
     if not os.path.exists(archive_path):
         return False
     try:
-        for host in env.hosts:
-            put(archive_path, '/tmp/')
-            archive_name = os.path.basename(archive_path)
-            exctraction_name = archive_name.split('.')[0]
-            run(f'sudo tar -xzf /tmp/{archive_name} -C /data/web_static/releases')
-            run('cd /data/web_static/releases')
-            run(f'sudo mv /data/web_static/releases/web_static /data/web_static/releases/{exctraction_name}')
-            run(f'sudo rm -r /tmp/{archive_name}')
-            run('sudo rm /data/web_static/current')
-            run(f'sudo ln -s /data/web_static/releases/{exctraction_name} /data/web_static/current')
-            return True
+        put(archive_path, '/tmp/')
+        archive_name = os.path.basename(archive_path)
+        exctraction_name = archive_name.split('.')[0]
+        run(f'sudo tar -xzf /tmp/{archive_name} -C /data/web_static/releases')
+        run('cd /data/web_static/releases')
+        old_path = '/data/web_static/releases/web_static'
+        new_path = f'/data/web_static/releases/{exctraction_name}'
+        run(f'sudo mv {old_path} {new_path}')
+        run(f'sudo rm -r /tmp/{archive_name}')
+        run('sudo rm /data/web_static/current')
+        run(f'sudo ln -s {new_path} /data/web_static/current')
+        return True
 
     except Exception:
         return False
