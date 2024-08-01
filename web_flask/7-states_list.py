7e053ce541c380e8d935f593e59902e67d959e15
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-01 21:22:30 +0300

    edit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     '''sends a page of states list'''
-    states_list = storage.all(State).values()
+    states_list = storage.all('State').values()
     states_list = sorted(states_list, key=lambda state: state.name)
     return render_template('7-states_list.html', states_list=states_list)
 
