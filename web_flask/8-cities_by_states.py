ea568d0112db7a619710530d596ee7e603f47d58
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-02 12:49:19 +0300

    edit

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,6 +19,7 @@ def teardown_db(exception):
 def cities_list():
     '''sends a page of states and cities list'''
     states_list = storage.all(State).values()
+    states_list = sorted(states_list, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states_list=states_list)
 
 
