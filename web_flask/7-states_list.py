401d48f625bfd3a882912cea29b134b366caa636
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-08-02 12:46:51 +0300

    edit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,7 +19,6 @@ def teardown_db(exception):
 def state_list():
     '''sends a page of states list'''
     states_list = storage.all(State).values()
-    states_list = sorted(states_list, key=lambda state: state.name)
     return render_template('7-states_list.html', states_list=states_list)
 
 
